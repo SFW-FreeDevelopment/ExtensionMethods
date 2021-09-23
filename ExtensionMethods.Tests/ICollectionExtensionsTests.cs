7d58027dd5d5529e0defc7ebc2dc53753fb11cb2
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-22 21:25:04 -0400

    Added unit tests for StringExtensions and ICollectionExtensions

---
+++ ExtensionMethods.Tests/ICollectionExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtensionMethods.Tests
+{
+    public class ICollectionExtensionsTests
+    {
+        [Fact]
+        public void IsNullOrEmpty_True_WhenValueIsNull()
+        {
+            ICollection value = null;
+            var result = value.IsNullOrEmpty();
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void IsNullOrEmpty_True_WhenValueIsEmpty()
+        {
+            ICollection value = new List<object>();
+            var result = value.IsNullOrEmpty();
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void IsNullOrEmpty_Value_WhenValueIsNotNullOrEmpty()
+        {
+            ICollection value = new List<object>() { new object() };
+            var result = value.IsNullOrEmpty();
+            
+            Assert.False(result);
+        }
+    }
+}
---
+++ ExtensionMethods.Tests/StringExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace ExtensionMethods.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void IsNotNullOrEmpty_False_WhenValueIsNull()
+        {
+            string value = null;
+            var result = value.IsNotNullOrEmpty();
+            
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void IsNotNullOrEmpty_False_WhenValueIsEmpty()
+        {
+            string value = "";
+            var result = value.IsNotNullOrEmpty();
+            
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void IsNotNullOrEmpty_True_WhenValueIsNotNullOrEmpty()
+        {
+            string value = "some value";
+            var result = value.IsNotNullOrEmpty();
+            
+            Assert.True(result);
+        }
+    }
+}
