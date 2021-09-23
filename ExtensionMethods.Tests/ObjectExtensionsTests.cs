772ba4ce47518e82119463d755663708b02f75a5
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-22 22:32:47 -0400

    Added ObjectExtensions and unit tests

---
+++ ExtensionMethods.Tests/ObjectExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace ExtensionMethods.Tests
+{
+    public class ObjectExtensionsTests
+    {
+        #region IsNull
+        [Fact]
+        public void IsNull_Returns_True_WhenValueIsNull()
+        {
+            object value = null;
+            bool result = value.IsNull();
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void IsNull_Returns_False_WhenValueIsNotNull()
+        {
+            object value = new object();
+            bool result = value.IsNull();
+            
+            Assert.False(result);
+        }
+        #endregion
+        
+        #region IsNotNull
+        [Fact]
+        public void IsNotNull_Returns_True_WhenValueIsNotNull()
+        {
+            object value = new object();
+            bool result = value.IsNotNull();
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void IsNotNull_Returns_False_WhenValueIsNull()
+        {
+            object value = null;
+            bool result = value.IsNotNull();
+            
+            Assert.False(result);
+        }
+        #endregion
+    }
+}
