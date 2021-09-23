a1350c2b0cdede989f26c531e14217695c1f94d1
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-22 21:16:42 -0400

    Added StringExtensions and ICollectionExtensions

---
+++ ExtensionMethods/ICollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace ExtensionMethods
+{
+    public static class ICollectionExtensions
+    {
+        public static bool IsNullOrEmpty(this ICollection value)
+        {
+            return value == null || value.Count == 0;
+        }
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static bool IsNotNullOrEmpty(this string value)
+        {
+            return value != null && value != string.Empty;
+        }
+    }
+}
