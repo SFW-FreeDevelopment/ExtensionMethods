cec4d839f76bedb7fb79c4bf54bf944f012d453d
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-22 22:06:14 -0400

    Added IEnumerableExtensions and unit tests

---
+++ ExtensionMethods.Tests/IEnumerableExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ExtensionMethods.Tests
+{
+    public class IEnumerableExtensionsTests
+    {
+       
+        [Fact]
+        public void IsNullOrEmpty_True_WhenValueIsNull()
+        {
+            IEnumerable<object> value = null;
+            var result = value.IsNullOrEmpty();
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void IsNullOrEmpty_True_WhenValueIsEmpty()
+        {
+            IEnumerable<object> value = new object[] { };
+            var result = value.IsNullOrEmpty();
+            
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void IsNullOrEmpty_Value_WhenValueIsNotNullOrEmpty()
+        {
+            IEnumerable<object> value = new object[] { new object() };
+            var result = value.IsNullOrEmpty();
+            
+            Assert.False(result);
+        }
+    }
+}
---
+++ ExtensionMethods/IEnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool IsNullOrEmpty(this IEnumerable<object> value)
+        {
+            return value == null || !value.Any();
+        }
+    }
+}
